837147075b0d224d7892e1b19b984e3fa17c813b
unknown <dpulaak@gmail.com>
2022-10-18 13:55:18 +0600

    First Commit

---
+++ laravelgigs/data.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'title' => "Laravel Senior Developer",
+    'tags' => "laravel, javascript",
+    'compant' => 'ACME corp',
+    'location' => 'Bostom, MA',
+    'email' => 'email@email.com',
+    'website' => 'https://www.acme.com',
+    'description' => 'lorem ipsisasjfhsadkjfhnsdajkfndskfnsdovdsajvoikdsanvdjskaf  vijsdfvhikujhesinufhsdjkfhdsfjksdbvnjkdfabufidajhgbdfiugbjkfgdnfbjkadfnoi'
+]
+
